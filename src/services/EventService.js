e6e4ba5141fd7a23333095ea675609afa69311a9
Ozan Alpay <ozanalpay@yandex.com.tr>
2019-02-24 23:12:44 +0100

    Progress Bar implemented with NProgress and Axios Interceptors

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import NProgress from 'nprogress'
 
 const apiClient = axios.create({
   baseURL: 'http://localhost:3000',
@@ -9,6 +10,25 @@ const apiClient = axios.create({
   }
 })
 
+apiClient.interceptors.request.use(config => {
+  NProgress.start()
+  return config
+})
+
+apiClient.interceptors.response.use(response => {
+  NProgress.done()
+  return response
+})
+/*
+Caveats:
+1 - Not Optimal for Multiple API Calls. --> Solution loading.js Vuex module
+2 - Template get loaded before API call is returned
+
+Interceptors good for:
+1 - On request to set authorization tokens.
+2 - On Response to format or filter data before it reaches into your app.
+3 - Catch 401 Responses
+*/
 export default {
   getEvents(perPage, page) {
     return apiClient.get('/events?_limit=' + perPage + '&_page=' + page)
